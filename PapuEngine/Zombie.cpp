a8c14f1191f638e27dd7572cce8a2acad62fb658
JuniorAlfredoHuerta <44213192+JuniorAlfredoHuerta@users.noreply.github.com>
2022-06-13 19:53:12 -0500

    Initial commit

---
+++ PapuEngine/Zombie.cpp
@@ -0,0 +1,25 @@
+#include "Zombie.h"
+
+Zombie::Zombie()
+{
+}
+
+
+Zombie::~Zombie()
+{
+}
+
+void Zombie::init( float speed, glm::vec2 position)
+{
+	_speed = speed;
+	_color.set(255, 255, 0, 255);
+	_position = position;
+	
+
+}
+
+void Zombie::update(const std::vector<std::string>& levelData)
+{
+
+	collideWithLevel(levelData);
+}
